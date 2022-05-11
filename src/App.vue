a63203258575ede34343a49c063642a5275d5a9f
Hilary Cheng <hilary.cheng@hungwai.com>
2022-05-11 21:53:33 +0800

    - Add Example for the Individual Movies Display Condition

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
     <div class="row align-items-center">
       <div class="col">
         <ul>
-          <li v-for="movie of movies" :key="movie.id" @click="showDetail = !showDetail">
+          <li v-for="movie of movies" :key="movie.id" @click="movie.showDetail = !movie.showDetail">
             <img  v-bind:src="'https://image.tmdb.org/t/p/w500' + movie.poster_path "  v-bind:alt="movie.title" class="img-responsive">
-            <h3 v-show="showDetail">{{ movie.title }}</h3>
-            <p v-show="showDetail">{{ movie.overview }}</p>
+            <h3 v-show="movie.showDetail">{{ movie.title }}</h3>
+            <p v-show="movie.showDetail">{{ movie.overview }}</p>
           </li>
         </ul>
       </div>
@@ -53,7 +53,17 @@ export default {
   methods: {
     loadData() {
       axios.get(BASE_URL + API_URL)
-        .then(response => this.movies = response.data.results)
+        .then(response => {
+          this.movies = response.data.results
+          for (let i = 0; i < this.movies.length; i++) {
+            // This is really javascript features only.
+            // It will add extra field to movie object at any time without restriction
+            // Don't do it Java or any other strong type Language
+            // Strong type means Variable Type must be will define at compile time
+            // Non-Strong type like Javascript, you can change the variable type at any time.
+            this.movies[i].showDetail = false
+          }
+        })
         .catch(error => console.log(error))
     }
   }
